965a7e6cf9076a8d3625afd36cb862c99552ce1a
Anton Stefanov Atanasov <funluiji@gmail.com>
2025-04-17 15:24:33 +0300

    changed libwebp dependency

--- Package.swift
+++ Package.swift
@@ -19,26 +19,22 @@ let package = Package(
         .package(url: "https://github.com/Quick/Quick.git", from: "6.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.0.0"),
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PostHog",
-            dependencies: ["phlibwebp"],
+            dependencies: [
+                .product(name: "libwebp", package: "libwebp-Xcode")
+            ],
             path: "PostHog",
             resources: [
                 .copy("Resources/PrivacyInfo.xcprivacy"),
             ]
         ),
-        .target(
-            name: "phlibwebp",
-            path: "vendor/libwebp",
-            publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath("."),
-            ]
-        ),
+
         .testTarget(
             name: "PostHogTests",
             dependencies: [
